92e231134b19274868854aec85ab275a85ad79ea
weinvade <vadewein@gmail.com>
2026-07-14 16:18:52 +0000

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -3,7 +3,7 @@
 # This script resets cgi-bin, node-modules
 # package.json and package-lock.json .
 
-rm -rf cgi-bin; rm -rf node-modules
-rm package.json; rm package-lock.json
+rm -rf cgi-bin node_modules
+rm package.json package-lock.json
 
 echo "Done."
